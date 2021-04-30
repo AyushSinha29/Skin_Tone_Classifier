37771ce76d450a199cf535f996c008a898ad3bde
AyushSinha29 <55970505+AyushSinha29@users.noreply.github.com>
2021-04-30 21:25:12 +0530

    Update module 3.py

--- module 3.py
+++ module 3.py
@@ -1,21 +1,20 @@
 from sklearn.cluster import KMeans
-import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+import cv2 as cv
 from collections import Counter
+from skimage.color import rgb2lab, deltaE_cie76
 from PIL import Image
 from numpy import asarray
 
-img = cv2.imread('C:/Users/KIIT/Downloads/SkinColor/detectedImage.png') 
-#it is the path of the extracted skin from Module 2.
+img = cv.imread('C:/Users/---------/XYZ.jpg') #path of original image
+image = cv.imread(r'C:\User-------------\detectedImage.png') #path of detected skin from module 2
 
+image = cv.cvtColor(image, cv.COLOR_BGR2RGB)
 
-img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+gray_image = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
 
-
-gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-
-
-resized_image = cv2.resize(img, (1200, 600))
+resized_image = cv.resize(image, (1200, 600))
 
 
 def RGB2HEX(color):
@@ -23,23 +22,20 @@ def RGB2HEX(color):
 
 
 def get_image(image_path):
-    image = cv2.imread(image_path)
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    image = cv.imread(image_path)
+    image = cv.cvtColor(image, cv.COLOR_BGR2RGB)
     return image
 
 
-
 # convert image to numpy array
-data = asarray(img)
+data = asarray(image)
 
 
 # create Pillow image
 image2 = Image.fromarray(data)
 
 
-
-
-modified_image = cv2.resize(img, (600, 400), interpolation = cv2.INTER_AREA)
+modified_image = cv.resize(image, (600, 400), interpolation = cv.INTER_AREA)
 modified_image = modified_image.reshape(modified_image.shape[0]*modified_image.shape[1], 3)
 
 
@@ -52,13 +48,8 @@ center_colors = clf.cluster_centers_
 # We get ordered colors by iterating through the keys
 ordered_colors = [center_colors[i] for i in counts.keys()]
 hex_colors = [RGB2HEX(ordered_colors[i]) for i in counts.keys()]
-rgb_colors = [ordered_colors[i] for i in counts.keys()]
+rgb_colors = [ordered_colors[i] for i in counts.keys()]   
 
-
-    
-###############################################################################   
-    
-#converting the hex color code into decimal 
 
 c1=hex_colors[0]
 c2=hex_colors[1]
@@ -72,36 +63,37 @@ res2 = int(d2, 16)
 if 8421504>res1>0: #to ignore the darker shade of the background
     fc=res2
     print("The detected skin tone is:",res2," with hex color code as",c2)
-    
+
 else:
     fc=res1
-    print("The detected skin tone is:",res1," with hex color code as",c1)  
-
-
+    print("The detected skin tone is:",res1,"(bg color) with hex color code as",c1)  
 
 
 print("The detected skintone is:")
+font = cv.FONT_HERSHEY_TRIPLEX
+
 if 16777215>fc>12619362: #fair range
     print("Fair")
-    font = cv2.FONT_HERSHEY_SIMPLEX 
-    cv2.putText(img,'Fair',(10,100), font, 1,(0,255,0),2)
-    cv2.imshow("Result",img)
-    cv2.waitKey(0)
+ 
+    cv.putText(img,'Skin Tone: FAIR',(10,50), font, 1,(0,255,0),2)
+    cv.imshow("Result",img)
+    cv.waitKey(0)
 
-    
-elif 12619362>fc>10450000:    #mild range
+
+elif 12619362>fc>10300000:    #mild range
     print("Mild")
-    font = cv2.FONT_HERSHEY_SIMPLEX
     
-    cv2.putText(img,'Mild',(10,100), font, 1,(0,255,0),2)
-    cv2.imshow("Result",img)
-    cv2.waitKey(0)
-    
+    cv.putText(img,'Skin Tone: MILD',(10,50), font, 0.5,(0,255,0),2)
+    cv.imshow("Result",img)
+    cv.waitKey(0)
+
 else:
     print("Dark")    
-    font = cv2.FONT_HERSHEY_SIMPLEX
-    
-    cv2.putText(img,'Dark',(10,100), font, 1,(0,255,0),2)
-    cv2.imshow("Result",img)
-    cv2.waitKey(0)
-    
+    cv.putText(img,'Skin Tone: DARK',(10,50), font, 0.5,(0,255,0),2)
+    cv.imshow("Result",image)
+    cv.waitKey(0)
+        
+
+  
+
+ 
